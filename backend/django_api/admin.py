dda749b036b5e619b696ba708b4e517d9bf10eb6
MuhdHishamP <hishamp2131@gmail.com>
2023-12-05 22:36:43 +0530

    Remove backend

--- backend/django_api/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import dash
-
-# Register your models here.
-admin.site.register(dash)
--- backend/django_api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import dash
-
-class dash_serializer(serializers.ModelSerializer):
-    class Meta:
-        model = dash
-        fields = '__all__'
